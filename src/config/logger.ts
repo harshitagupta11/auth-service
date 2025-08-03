b6572b20464a18fba7574f55c258b65668a9dd8d
Harshita Gupta <guptaharshita99@gmail.com>
2025-08-03 11:31:18 +0530

    timestamp for all logs

--- src/config/logger.ts
+++ src/config/logger.ts
@@ -4,23 +4,19 @@ import { Config } from '.';
 const logger = winston.createLogger({
     level: 'info',
     defaultMeta: { serviceName: 'auth-service' },
+    format: winston.format.combine(
+        winston.format.timestamp(),
+        winston.format.json(),
+    ),
     transports: [
         new winston.transports.File({
             level: 'info',
-            format: winston.format.combine(
-                winston.format.timestamp(),
-                winston.format.json(),
-            ),
             dirname: 'logs',
             filename: 'app.log',
             silent: false,
         }),
         new winston.transports.File({
             level: 'error',
-            format: winston.format.combine(
-                winston.format.timestamp(),
-                winston.format.json(),
-            ),
             dirname: 'logs',
             filename: 'error.log',
             silent: true,
@@ -28,10 +24,6 @@ const logger = winston.createLogger({
 
         new winston.transports.Console({
             level: 'info',
-            format: winston.format.combine(
-                winston.format.timestamp(),
-                winston.format.json(),
-            ),
             silent: Config.NODE_ENV === 'production', // Disable console logging in production
         }),
     ],
